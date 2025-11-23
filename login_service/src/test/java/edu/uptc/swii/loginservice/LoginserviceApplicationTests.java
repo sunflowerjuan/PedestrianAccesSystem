359e1dbf9d8ff1b18802423935b9157a7e451e03
sunflowersjuan <sebasjuan331@gmail.com>
2025-11-22 22:43:31 -0500

    APP services

---
+++ login_service/src/test/java/edu/uptc/swii/loginservice/LoginserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uptc.swii.loginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
